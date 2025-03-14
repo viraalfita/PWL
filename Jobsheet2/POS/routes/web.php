01d0d8ae6fd22017e630a04e4308f1e5650bcbcd
viraalfita <viraalfita1813@gmail.com>
2025-03-14 20:21:02 +0700

    praktikum 4

--- Jobsheet2/POS/routes/web.php
+++ Jobsheet2/POS/routes/web.php
@@ -3,18 +3,28 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+// JOBSHEET 2
+// Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [CategoryController::class, 'foodBeverage'])->name('category.food-beverage');
-    Route::get('/beauty-health', [CategoryController::class, 'beautyHealth'])->name('category.beauty-health');
-    Route::get('/home-care', [CategoryController::class, 'homeCare'])->name('category.home-care');
-    Route::get('/baby-kid', [CategoryController::class, 'babyKid'])->name('category.baby-kid');
+// Route::prefix('category')->group(function () {
+//     Route::get('/food-beverage', [CategoryController::class, 'foodBeverage'])->name('category.food-beverage');
+//     Route::get('/beauty-health', [CategoryController::class, 'beautyHealth'])->name('category.beauty-health');
+//     Route::get('/home-care', [CategoryController::class, 'homeCare'])->name('category.home-care');
+//     Route::get('/baby-kid', [CategoryController::class, 'babyKid'])->name('category.baby-kid');
+// });
+
+// Route::get('/user/{id}/name/{name}', [UserController::class, 'profile'])->name('user.profile');
+
+// Route::get('/sales', [SalesController::class, 'index'])->name('sales.index');
+
+// JOBSHEET 3
+
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/user/{id}/name/{name}', [UserController::class, 'profile'])->name('user.profile');
-
-Route::get('/sales', [SalesController::class, 'index'])->name('sales.index');
+Route::get('/level', [LevelController::class, 'index']);
