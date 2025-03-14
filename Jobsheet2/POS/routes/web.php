3a7a40d670ee6adf138fce59eef0bf7036a38b1e
viraalfita <viraalfita1813@gmail.com>
2025-03-14 20:43:34 +0700

    praktikum 5

--- Jobsheet2/POS/routes/web.php
+++ Jobsheet2/POS/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
@@ -28,3 +29,4 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
