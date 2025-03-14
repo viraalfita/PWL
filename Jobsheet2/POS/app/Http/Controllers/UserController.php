437f58ebb541fe2bb2bf75caada7627234ff4bd0
viraalfita <viraalfita1813@gmail.com>
2025-03-14 21:14:42 +0700

    praktikum 6 dan praktikum 7

--- Jobsheet2/POS/app/Http/Controllers/UserController.php
+++ Jobsheet2/POS/app/Http/Controllers/UserController.php
@@ -2,12 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function profile($id, $name)
+    // public function profile($id, $name)
+    // {
+    //     return view('user.profile', compact('id', 'name'));
+    // }
+
+    public function index()
     {
-        return view('user.profile', compact('id', 'name'));
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
+        // ];
+
+        // UserModel::insert($data);
+
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+
+        UserModel::where('username', 'customer-1')->update($data);
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
     }
 }
--- Jobsheet2/POS/routes/web.php
+++ Jobsheet2/POS/routes/web.php
@@ -30,3 +30,4 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
