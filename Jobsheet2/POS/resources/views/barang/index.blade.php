bbeb0602e644fb7ecc1d9ee48424f580f5d82897
viraalfita <viraalfita1813@gmail.com>
2025-03-29 19:13:08 +0700

    tugas: barang

--- Jobsheet2/POS/resources/views/barang/index.blade.php
+++ Jobsheet2/POS/resources/views/barang/index.blade.php
@@ -1,119 +1,91 @@
 @extends('layouts.template')
 
 @section('content')
-<div class="card card-outline card-primary">
-    <div class="card-header">
-        <h3 class="card-title">{{ $page->title }}</h3>
-        <div class="card-tools">
-            <a class="btn btn-sm btn-primary mt-1" href="{{ url('barang/create') }}">Tambah</a>
-        </div>
-    </div>
-    <div class="card-body">
-    @if (session("success"))
-        <div class="alert alert-success">{{ session("success") }}</div>
-    @endif
-
-    @if (session('error'))
-        <div class="alert alert-danger">{{ session('error') }}</div>
-    @endif
-
-    <div class="row">
-        <div class="col-md-12">
-            <div class="form-group row">
-                <label class="col-1 control-label col-form-label">Filter:</label>
-                <div class="col-3">
-                    <select class="form-control" id="kategori_id" name="kategori_id" required>
-                        <option value="">- Semua -</option>
-                        @foreach($kategori as $item)
-                            <option value="{{ $item->kategori_id }}">{{ $item->kategori_nama }}</option>
-                        @endforeach
-                    </select>
-                    <small class="form-text text-muted">Level Pengguna</small>
-                </div>
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3 class="card-title">{{ $page->title }}</h3>
+            <div class="card-tools">
+                <a class="btn btn-sm btn-primary mt-1" href="{{ url('barang/create') }}">Tambah</a>
+                <button onclick="modalAction('{{ url('barang/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah
+                    Ajax</button>
             </div>
         </div>
+        <div class="card-body">
+            @if (session('success'))
+                <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+
+            @if (session('error'))
+                <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_barang">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Kode Barang</th>
+                        <th>Nama Barang</th>
+                        <th>Kategori</th>
+                        <th>Harga Beli</th>
+                        <th>Harga Jual</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
     </div>
-
-    <table class="table table-bordered table-striped table-hover table-sm" id="table_barang">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Kategori</th>
-                <th>Kode Barang</th>
-                <th>Barang</th>
-                <th>Harga Beli</th>
-                <th>Harga Jual</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-    </table>
-</div>
-</div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
+        data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
-@push('css')
-@endpush
-@push('js')
-<script>
-    $(document).ready(function() {
-      var dataBarang = $('#table_barang').DataTable({
-            serverSide: true,
-            ajax: {
-                url: "{{ url('barang/list') }}",
-                type: "get",
-                dataType: "json",
-                data: function (d) {
-                    d.kategori_id = $('#kategori_id').val();
-                }
-            },
-            columns: [
-                {
-                    data: "DT_RowIndex",
-                    className: "text-center",
-                    orderable: false,
-                    searchable: false
-                },
-                {
-                    data: "kategori.kategori_nama",
-                    className: "",
-                    orderable: true,
-                    searchable: true
-                },
-                {
-                    data: "barang_kode",
-                    className: "",
-                    orderable: true,
-                    searchable: true
-                },
-                {
-                    data: "barang_nama",
-                    className: "",
-                    orderable: false,
-                    searchable: false
-                },
-                {
-                    data: "harga_beli",
-                    className: "",
-                    orderable: false,
-                    searchable: false
-                },
-                {
-                    data: "harga_jual",
-                    className: "",
-                    orderable: false,
-                    searchable: false
-                },
-                {
-                    data: "aksi",
-                    className: "",
-                    orderable: false,
-                    searchable: false
-                }
-            ]
-        });
 
-        $('#kategori_id').on('change', function () {
-            dataBarang.ajax.reload();
-        })
-    });
-</script>
+@push('js')
+    <script>
+        function modalAction(url = '') {
+            $('#myModal').load(url, function() {
+                $('#myModal').modal('show');
+            });
+        }
+
+        let dataBarang;
+        $(document).ready(function() {
+            dataBarang = $('#table_barang').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ url('barang') }}",
+                columns: [{
+                        data: 'DT_RowIndex',
+                        className: 'text-center',
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: 'barang_kode',
+                        className: 'text-center'
+                    },
+                    {
+                        data: 'barang_nama',
+                        className: 'text-center'
+                    },
+                    {
+                        data: 'kategori.kategori_nama',
+                        className: 'text-center'
+                    },
+                    {
+                        data: 'harga_beli',
+                        className: 'text-center'
+                    },
+                    {
+                        data: 'harga_jual',
+                        className: 'text-center'
+                    },
+                    {
+                        data: 'aksi',
+                        className: 'text-center',
+                        orderable: false,
+                        searchable: false
+                    }
+                ]
+            });
+        });
+    </script>
 @endpush
