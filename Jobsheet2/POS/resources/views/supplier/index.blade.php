80b9ab85ff6c63bfd42cb3ebbb6be4baf3900ece
viraalfita <viraalfita1813@gmail.com>
2025-03-29 19:06:04 +0700

    tugas: supplier

--- Jobsheet2/POS/resources/views/supplier/index.blade.php
+++ Jobsheet2/POS/resources/views/supplier/index.blade.php
@@ -1,81 +1,81 @@
 @extends('layouts.template')
 
 @section('content')
-<div class="card card-outline card-primary">
-    <div class="card-header">
-        <h3 class="card-title">{{ $page->title }}</h3>
-        <div class="card-tools">
-            <a class="btn btn-sm btn-primary mt-1" href="{{ url('supplier/create') }}">Tambah</a>
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3 class="card-title">{{ $page->title }}</h3>
+            <div class="card-tools">
+                <a class="btn btn-sm btn-primary mt-1" href="{{ url('supplier/create') }}">Tambah</a>
+                <button onclick="modalAction('{{ url('supplier/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah
+                    Ajax</button>
+            </div>
+        </div>
+        <div class="card-body">
+            @if (session('success'))
+                <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+
+            @if (session('error'))
+                <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_supplier">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Kode</th>
+                        <th>Nama</th>
+                        <th>Alamat</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+            </table>
         </div>
     </div>
-    <div class="card-body">
-    @if (session("success"))
-        <div class="alert alert-success">{{ session("success") }}</div>
-    @endif
-
-    @if (session('error'))
-        <div class="alert alert-danger">{{ session('error') }}</div>
-    @endif
-
-    <table class="table table-bordered table-striped table-hover table-sm" id="table_supplier">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Supplier Kode</th>
-                <th>Supplier Nama</th>
-                <th>Supplier Alamat</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-    </table>
-</div>
-</div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
+        data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
-@push('css')
-@endpush
+
 @push('js')
-<script>
-    $(document).ready(function() {
-      var dataSupplier = $('#table_supplier').DataTable({
-            serverSide: true,
-            ajax: {
-                url: "{{ url('supplier/list') }}",
-                type: "get",
-                dataType: "json",
-            },
-            columns: [
-                {
-                    data: "DT_RowIndex",
-                    className: "text-center",
-                    orderable: false,
-                    searchable: false
-                },
-                {
-                    data: "supplier_kode",
-                    className: "",
-                    orderable: true,
-                    searchable: true
-                },
-                {
-                    data: "supplier_nama",
-                    className: "",
-                    orderable: true,
-                    searchable: true
-                },
-                {
-                    data: "supplier_alamat",
-                    className: "",
-                    orderable: true,
-                    searchable: true
-                },
-                {
-                    data: "aksi",
-                    className: "",
-                    orderable: false,
-                    searchable: false
-                }
-            ]
+    <script>
+        function modalAction(url = '') {
+            $('#myModal').load(url, function() {
+                $('#myModal').modal('show');
+            });
+        }
+        let dataSupplier;
+        $(document).ready(function() {
+            dataSupplier = $('#table_supplier').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ url('/supplier') }}",
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex',
+                        orderable: false,
+                        searchable: false,
+                        className: 'text-center'
+                    },
+                    {
+                        data: 'supplier_kode',
+                        name: 'supplier_kode'
+                    },
+                    {
+                        data: 'supplier_nama',
+                        name: 'supplier_nama'
+                    },
+                    {
+                        data: 'supplier_alamat',
+                        name: 'supplier_alamat'
+                    },
+                    {
+                        data: 'aksi',
+                        name: 'aksi',
+                        orderable: false,
+                        searchable: false
+                    }
+                ]
+            });
         });
-    });
-</script>
+    </script>
 @endpush
